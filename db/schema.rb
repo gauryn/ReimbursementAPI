248ab9f16bf7db54d0650dcca6d7ac549cc55a9c
gauryn <gauryn22@gmail.com>
2016-12-09 03:04:38 -0500

    working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208071823) do
+ActiveRecord::Schema.define(version: 20161206041659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,13 +33,6 @@ ActiveRecord::Schema.define(version: 20161208071823) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "receipts", force: :cascade do |t|
-    t.string   "receipt_images"
-    t.string   "reimbursement_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "reimbursements", force: :cascade do |t|
     t.decimal  "total"
     t.string   "description"
@@ -50,7 +43,7 @@ ActiveRecord::Schema.define(version: 20161208071823) do
     t.string   "event_location"
     t.integer  "num_of_attendees"
     t.string   "organization"
-    t.string   "receipt_images"
+    t.text     "receipt_images"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
